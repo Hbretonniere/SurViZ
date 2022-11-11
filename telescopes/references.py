d9ef4529c10b968b328da37540a67e14dffb848c
Hubert Bretonniere <hubert.bretonniere@ias.u-psud.fr>
2022-11-11 09:55:03 -0800

    adding referecnes

---
+++ telescopes/references.py
@@ -0,0 +1,40 @@
+filters_refs = """
+References:
+- Euclid:
+    - NISP: https://sci.esa.int/web/euclid/-/euclid-nisp-instrument
+    - VIS: https://sci.esa.int/web/euclid/-/euclid-vis-instrument
+- JWST:
+    - NIRCAM: https://jwst-docs.stsci.edu/jwst-near-infrared-camera/nircam-instrumentation/nircam-filters
+- HST:
+    - ACS: https://etc.stsci.edu/etcstatic/users_guide/appendix_b_acs.html
+- Rubin:
+    - https://ui.adsabs.harvard.edu/abs/2009arXiv0912.0201L
+- SDSS: https://www.astro.princeton.edu/PBOOK/camera/camera.htm 
+"""
+
+mirrors_refs = """
+References:
+- Euclid: 
+    - Mirror: https://sci.esa.int/web/euclid/-/57042-euclid-primary-mirror
+    - FOV: https://sci.esa.int/web/euclid/-/euclid-vis-instrument
+- JWST:
+    - Mirror: https://webb.nasa.gov/content/observatory/ote/mirrors/index.html
+    - FOV: https://jwst-docs.stsci.edu/jwst-near-infrared-camera
+    
+- HST: 
+    - Mirror: https://www.nasa.gov/content/goddard/hubble-space-telescope-optics-system
+    - FOV: https://www.nasa.gov/content/hubble-space-telescope-advanced-camera-for-surveys
+- SDSS: 
+    - Mirror and FOV: https://www.sdss.org/instruments/
+- Rubin LSST:
+    - Mirror: https://www.lsst.org/about/tel-site/mirror
+    - FOV: https://www.lsst.org/about/camera
+
+"""
+
+footprint_refs = """
+References:
+- SDSS:
+    - SDSS-I: https://classic.sdss.org/dr7/coverage/index.html
+    
+    """
