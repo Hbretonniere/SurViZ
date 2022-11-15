07e847c823c72f179f624acb7a4e7d27873c27db
Hubert Bretonniere <hubert.bretonniere@ias.u-psud.fr>
2022-11-14 18:23:37 -0800

    adding fields and gal routines

--- utils/diverse_utils.py
+++ utils/diverse_utils.py
@@ -1,6 +1,15 @@
 import numpy as np
 from astropy.coordinates import SkyCoord
 import astropy.units as u
+import galsim 
+import glob 
+import sys
+from astropy.io import fits
+
+def MagToFlux(zp, mag):
+    Flux = np.power(10, ((zp - mag)*0.4))
+    return Flux
+
 
 def create_psf(fwhm, pxscale):
 
@@ -60,3 +69,93 @@ def eq2gal(ra, dec):
     l_gal, b_gal = -gal.l.wrap_at('180d').radian, gal.b.radian
     
     return l_gal, b_gal
+
+from csv import excel_tab
+
+
+def sim_field(cat, pxscale, fwhm, noise_level, zp):
+
+    catName = glob.glob('../data/vis_0*')[0]
+    cat = galsim.Catalog(catName) 
+    ### NOISE LEVEL IN MICROJANSKY
+    divideFor = 40
+    NumGal = 314*1000/(divideFor**2)  # 314709 in the list
+    image_size_x = int(25000/divideFor)
+    image_size_y = int(25000/divideFor)
+    full_image = galsim.ImageF(image_size_x, image_size_y)
+    for i in range(int(NumGal)):
+        if i == 184:
+            continue
+        stamp_size = 1000
+        stamp_gal = galsim.ImageF(stamp_size, stamp_size, scale=pxscale)
+        gal_idx = i
+        float_ix = cat.getFloat(gal_idx, 1) / divideFor
+        float_iy = cat.getFloat(gal_idx, 2) / divideFor
+        radiusBulge = cat.getFloat(i, 5)
+        radiusDisk = cat.getFloat(i, 8)*1.678
+        bt = cat.getFloat(i, 4)
+        ell_B = cat.getFloat(i, 6)
+        ell_D = cat.getFloat(i, 9)
+        q = bt*ell_B + (1-bt)*ell_D
+        mag = cat.getFloat(i, 3)
+        Flux = MagToFlux(zp, mag)
+        PA = cat.getFloat(i, 7)
+        ix = int(cat.getFloat(i, 1) / divideFor)
+        iy = int(cat.getFloat(i, 2) / divideFor)
+        dx = float_ix - ix
+        dy = float_iy - iy
+        offset = galsim.PositionD(dx, dy)
+        bulge = galsim.Sersic(4, radiusBulge, flux=1.0)
+        b_shear = galsim.Shear(q=ell_B, beta=galsim.Angle(PA, galsim.radians))
+        bulge = bulge.shear(b_shear)
+        
+        disk = galsim.Sersic(1, radiusDisk, flux=1.0)
+        d_shear = galsim.Shear(q=ell_D, beta=galsim.Angle(PA, galsim.radians))
+        disk = disk.shear(d_shear)
+        gal = bulge + disk
+        psf = galsim.Gaussian(flux=1., fwhm=fwhm)
+        gal = galsim.Convolve(psf, gal)
+        gal = gal.withFlux(Flux)
+        try:
+            stamp = gal.drawImage(stamp_gal, method='no_pixel')
+        except Exception:
+            print(i, radiusBulge, radiusDisk)
+            continue
+        stamp.setCenter(ix, iy)
+        bounds = stamp.bounds & full_image.bounds
+        full_image[bounds] += stamp[bounds]
+        full_image += np.random.normal(0, noise_level, (image_size_x, image_size_y))
+    return full_image.array
+
+def sim_and_save(cat, telescope, instrument, survey):
+    "Example: sim_and_save('HST', 'ACS', 'HST-Cosmos')"
+
+    sys.path.append('../')
+    from telescopes.main_info import info
+    instrument_info = info[telescope]['surveys'][survey]['instruments'][instrument]
+    band = instrument_info['main_band']
+    image = sim_field(cat, instrument_info['pix_scale'],
+                instrument_info['bands'][band]['fwhm'],
+                info[telescope]['surveys'][survey]['std_noise'],
+                instrument_info['bands'][band]['zp'])
+    
+    fits.writeto(f'../data/fields/{telescope}_{instrument}_{survey}.fits', image, overwrite=True)
+
+def create_and_save_gal(info, telescope, instrument, show=False):
+    import cv2
+    from scipy.signal import convolve2d
+
+    survey = list(info[telescope]['surveys'].keys())[0]
+    instrument_info = info[telescope]['surveys'][survey]['instruments'][instrument]
+    pixel_scale = instrument_info['pix_scale']
+
+    band = instrument_info['main_band']
+    fwhm = instrument_info['bands'][band]['fwhm']
+    print(pixel_scale, fwhm)
+    img = np.load('../data/tng_gal.npy')[10:-1, 10:-10]
+    s = int(237 * 0.03/pixel_scale)
+    size = (s, s)
+    psf = create_psf(fwhm, pixel_scale)
+    psfed = convolve2d(img, psf, mode='same')
+    res = cv2.resize(psfed, dsize=size, interpolation=cv2.INTER_CUBIC)
+    np.save(f'../data/individual_gals/gal_{telescope}_{instrument}.npy', res)
